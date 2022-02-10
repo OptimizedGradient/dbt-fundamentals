d73f537dbebe7337e00d9ebddeddd33697df59f5
Dakota Kelley <dkelley@phdata.io>
2022-02-10 19:31:28 +0000

    Update orders_by_day analysis

--- analysis/orders_by_day.sql
+++ analysis/orders_by_day.sql
@@ -7,12 +7,12 @@ daily AS (
     SELECT order_date,
            COUNT(*) AS order_num,
            -- Jinja pivot out totals by status
-           {% for order_status in ['returned', 'completed', 'return_pending', 'shipped', 'placed'] %}
-                SUM(CASE 
-                        WHEN status = '{{ order_status }}' THEN 1
-                        ELSE 0
-                    END) AS {{ order_status }}_total {{ ',' if not loop.last}}
-           {% endfor %}
+        {% for order_status in ['returned', 'completed', 'return_pending', 'shipped', 'placed'] %}
+            SUM(CASE 
+                    WHEN status = '{{ order_status }}' THEN 1
+                    ELSE 0
+                END) AS {{ order_status }}_total {{ ',' if not loop.last}}
+        {% endfor %}
       FROM orders
      GROUP BY 1
 ),
