2a790ff7a3d208684d00b1d382a74ef1126b84bf
Dakota Kelley <dkelley@phdata.io>
2022-02-10 21:05:43 +0000

    Parameterized macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,6 +3,6 @@ SELECT id AS payment_id,
        paymentmethod,
        status,
        -- amounts are in cents
-       {{ cents_to_dollars() }} AS amount,
+       {{ cents_to_dollars('amount') }} AS amount,
        created AS created_at
   FROM {{ source('stripe', 'payment') }}
