941266944d9d1d3f9b9825c9ec46699b472d45c0
Dakota Kelley <dkelley@phdata.io>
2022-02-10 20:51:08 +0000

    Used jinja to created a pivoted orders payment matrix

---
+++ models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,27 @@
+{# Setup and order a list of our payments we want to pivot #}
+{% set jinja_payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] %}
+
+-- gets base payments
+WITH payments AS (
+  SELECT *
+    FROM {{ ref('stg_payments') }}
+),
+
+-- pivots payments with jinja
+pivot_payment_type AS (
+  SELECT order_id,
+         -- jinja pivot out amounts by order id and payment method
+      {% for paymentmethod in jinja_payment_methods %}
+         SUM(CASE
+                 WHEN paymentmethod = '{{ paymentmethod }}' THEN amount
+                 ELSE 0
+             END) AS {{ paymentmethod }}_sum {{ ',' if not loop.last }}
+      {% endfor %}
+    FROM payments
+   WHERE status = 'success'
+   GROUP BY order_id
+)
+
+-- get pivoted data
+SELECT *
+  FROM pivot_payment_type
